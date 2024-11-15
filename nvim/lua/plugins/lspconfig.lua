952d46132ab3a82ac8bafc229f334353ae31c2bc
akane <qweasd28848@gmail.com>
2024-11-15 20:40:39 +0800

    Add lspconfig key mapping

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,15 +3,34 @@ return {
   dependencies = { "hrsh7th/cmp-nvim-lsp" },
   config = function()
     local lspconfig = require("lspconfig")
-	local capabilities = require "cmp_nvim_lsp".default_capabilities()
-	-- Add other language server setups as needed
+    local capabilities = require "cmp_nvim_lsp".default_capabilities()
+
+    -- Define custom key mappings in the on_attach function
+    local on_attach = function(client, bufnr)
+      -- Buffer-local key mapping options
+      local opts = { noremap = true, silent = true, buffer = bufnr }
+
+      -- Key mappings
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)        -- Go to definition
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)       -- Go to declaration
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)              -- Show hover docs
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)    -- Rename symbol
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts) -- Code actions
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)        -- Show references
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)      -- Previous diagnostic
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)      -- Next diagnostic
+    end
+
+    -- Add other language server setups as needed
     -- Example: lspconfig.pyright.setup{}
     lspconfig.clangd.setup{
-		capabilities = capabilities
-	}
-	lspconfig.lua_ls.setup{
-		capabilities = capabilities
-	}
+      capabilities = capabilities,
+      on_attach = on_attach
+    }
+    lspconfig.lua_ls.setup{
+      capabilities = capabilities,
+      on_attach = on_attach
+    }
 
     -- Optional: Customize LSP diagnostics
     vim.diagnostic.config({
