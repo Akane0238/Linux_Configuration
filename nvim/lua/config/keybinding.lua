f0d46659bd1cc54ee1eb9231fd40693d28af9781
akane <qweasd28848@gmail.com>
2024-11-25 15:07:33 +0800

    Remove go folder

--- nvim/lua/config/keybinding.lua
+++ nvim/lua/config/keybinding.lua
@@ -5,4 +5,4 @@ vim.g.mapleader = " "
 keymap.set('n', '<leader>w', ':w<CR>', {desc = 'Save'})
 keymap.set('n', '<leader>q', ':q<CR>', {desc = 'Quit'})
 keymap.set({'i','c'}, 'jk', '<Esc>')
-keymap.set('n', '9', '$')
+keymap.set({'n','v'}, '9', '$')
